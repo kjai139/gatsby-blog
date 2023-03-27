aeafa1262669778073679af494e3ff9589d86cba
simon y <71997308+kjai139@users.noreply.github.com>
2023-03-26 21:52:28 -0400

    gatsby tutorial

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <div>
+        <p>hello tehre</p>
+      </div>
+    </Layout>
+  )
+}
+
+export const Head = () => <title>About Me</title>
+
+export default AboutPage
